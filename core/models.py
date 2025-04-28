d829865467d9e0ed3a0ca18590e33c95582ba0de
qmuuoryan <ryanmuuo91@gmail.com>
2025-04-28 13:04:32 +0300

    created models for the project

--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class HealthProgram(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+        
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+    enrolled_programs = models.ManyToManyField(HealthProgram, blank=True)
+
+    def __str__(self):
+        return self.name
