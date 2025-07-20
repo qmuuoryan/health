4640c0a0dfc5641818c4bfe5ab17d8851aa0812a
qmuuoryan <ryanmuuo91@gmail.com>
2025-07-21 00:05:45 +0300

    added my serializers

--- core/serializers.py
+++ core/serializers.py
@@ -1,19 +1,24 @@
 from rest_framework import serializers
-from .models import HealthProgram, Client
+from .models import Client, HealthProgram, Enrollment
 
 class HealthProgramSerializer(serializers.ModelSerializer):
     class Meta:
         model = HealthProgram
         fields = '__all__'
 
+class EnrollmentSerializer(serializers.ModelSerializer):
+    program = HealthProgramSerializer(read_only=True)
+    program_id = serializers.PrimaryKeyRelatedField(
+        queryset=HealthProgram.objects.all(), source='program', write_only=True
+    )
+
+    class Meta:
+        model = Enrollment
+        fields = ['id', 'program', 'program_id', 'enrolled_on']
+
 class ClientSerializer(serializers.ModelSerializer):
-    enrolled_programs = HealthProgramSerializer(many=True, read_only=True)
+    enrollments = EnrollmentSerializer(source='enrollment_set', many=True, read_only=True)
 
     class Meta:
         model = Client
-        fields = '__all__'
-
-class ClientEnrollSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Client
-        fields = ['enrolled_programs']
+        fields = ['id', 'name', 'age', 'gender', 'contact_info', 'enrollments']
