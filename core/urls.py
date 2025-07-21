bf295d8807a8b2de69804ace6a208d85be810d96
qmuuoryan <ryanmuuo91@gmail.com>
2025-07-21 23:50:37 +0300

    registration and login urls

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ClientViewSet, HealthProgramViewSet
+from django.contrib.auth import views as auth_views
+from . import views
 
 router = DefaultRouter()
 router.register(r'clients', ClientViewSet)
@@ -8,4 +10,10 @@ router.register(r'programs', HealthProgramViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', views.home, name='home'),
+    path('register/', views.register_client, name='register_client'),
+    path('clients/', views.client_list, name='client_list'),
+    path('enroll/<int:client_id>/', views.enroll_client, name='enroll_client'),
 ]
